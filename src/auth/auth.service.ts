b6ba26672f2e04b8329b96b2617e49961476898f
freddy <freddy.le.goldenowl@gmail.com>
2024-11-25 15:58:31 +0700

    feat: implement user login functionality and add user login DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { UserCreateDTO } from 'src/users/dto';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { UserLoginDTO } from './dto/user-login.dto';
+import { UserRegisterDTO } from './dto';
 import { UsersService } from 'src/users/users.service';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async register(userRegisterDTO: UserCreateDTO) {
+  async register(userRegisterDTO: UserRegisterDTO) {
     await this.usersService.create(userRegisterDTO);
     return;
+  }
+
+  async login(userLoginDTO: UserLoginDTO) {
+    const user = await this.usersService.findOne({ email: userLoginDTO.email });
+    if (!user) throw new NotFoundException('User not found');
+
+    const isPasswordMatch = await compare(userLoginDTO.password, user.password);
+    if (!isPasswordMatch) throw new ConflictException('Invalid password');
+
+    return user;
   }
 }
