55bc1a743cc7b018e8921b1265e73650c5f5fa29
freddy <freddy.le.goldenowl@gmail.com>
2024-11-25 14:38:08 +0700

    feat: enable CORS with specific origin and method restrictions

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,20 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      // Check if the origin is either localhost:3000 or matches the regex for golden-owl
+      if (typeof origin === 'undefined' || origin === 'http://localhost:3000') {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject the request
+      }
+    },
+    methods: 'GET,POST,PUT,PATCH,DELETE', // Allow specific HTTP methods (optional)
+    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers (optional)
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
