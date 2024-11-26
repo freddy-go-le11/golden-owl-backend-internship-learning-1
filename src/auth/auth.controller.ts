e3c5eceda9d6c1dcd1dff170747581af9138df9a
freddy <freddy.le.goldenowl@gmail.com>
2024-11-26 11:52:50 +0700

    feat: add logout and refresh token functionality to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,22 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { UserLoginDTO, UserRegisterDTO } from './dto';
+import { CustomizeJwtService } from 'src/jwt/jwt.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private jwtService: CustomizeJwtService,
+  ) {}
 
   @Post('register')
   async register(@Body() userRegisterDTO: UserRegisterDTO) {
@@ -21,19 +32,55 @@ export class AuthController {
     const { accessToken, refreshToken } =
       await this.authService.login(userLoginDTO);
 
+    const currentDate = new Date();
+
     res.cookie('refreshToken', refreshToken, {
       httpOnly: false,
       secure: true,
       path: '/',
       sameSite: 'none',
+      expires: new Date(currentDate.getTime() + 1000 * 60 * 60 * 24 * 1),
     });
     res.cookie('accessToken', accessToken, {
       httpOnly: false,
       secure: true,
       path: '/',
       sameSite: 'none',
+      expires: new Date(currentDate.getTime() + 1000 * 60 * 60 * 24 * 7),
     });
 
     return;
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('refreshToken');
+    res.clearCookie('accessToken');
+    return;
+  }
+
+  @Post('refresh-token')
+  async refreshToken(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = req.cookies['refreshToken'];
+    console.log('🚀 ~ AuthController ~ refreshToken:', refreshToken);
+    if (!refreshToken)
+      throw new BadRequestException('No refresh token provided');
+    const accessToken = req.cookies['accessToken'];
+    if (accessToken) return;
+
+    const newAccessToken =
+      this.jwtService.getAccessTokenFromRefreshToken(refreshToken);
+
+    res.cookie('accessToken', newAccessToken, {
+      httpOnly: false,
+      secure: true,
+      path: '/',
+      sameSite: 'none',
+    });
+
+    return res;
   }
 }
